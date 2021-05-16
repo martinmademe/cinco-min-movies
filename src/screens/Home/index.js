d0b4b5b44861b7d5cacdb00d92cddde002c8c4a4
Martin Rio Sixto <martin@martinmade.me>
2021-05-16 18:00:04 +0100

    Fetch search queries

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -7,16 +7,31 @@ import Terms from 'app/components/Terms'
 import List from 'app/components/List'
 
 import MOCK from 'app/mock.json'
-// import { API_KEY, POSTER_URL } from '@env'
+import { API_KEY, API_URL, POSTER_URL } from '@env'
 
 const HomeScreen = ({ navigation }) => {
   const [searchText, setSearchText] = useState(null)
+  const [movieData, setMovieData] = useState(MOCK)
+
+  const fetchMovies = async (params) => {
+
+    const url = `${API_URL}api_key=${API_KEY}&language=en-US&query=${params}&page=1&include_adult=false`
+
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      return setMovieData(data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Layout>
       <Search setSearchText={setSearchText} />
       <Terms text={searchText} />
-      <List data={MOCK.results} />
+      <List data={movieData} />
+      <Button title='SEARCH' onPress={() => fetchMovies(searchText)} />
 
       <Button
         title="Go to Details"
